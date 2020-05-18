8646bb2c5afb45aae6a060070c003fe8c866d945
Rahul vats <rahulvats1996@gmail.com>
2020-05-18 17:16:58 +0530

    Updates

---
+++ precache-manifest.f4d320b2e79c7f794db6d2fb4be2c8f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95ed276d32d1d77ca525",
+    "url": "/portfolio/static/css/main.303b85d2.chunk.css"
+  },
+  {
+    "revision": "95ed276d32d1d77ca525",
+    "url": "/portfolio/static/js/main.95ed276d.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b4e43b6221ed015ffa12fc9744fc5291",
+    "url": "/portfolio/index.html"
+  }
+];
